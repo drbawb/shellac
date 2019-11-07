ca3b41f10b840bb2790b3cc6ead6e16cc603106c
drbawb <drbawb@fatalsyntax.com>
2019-11-07 16:33:08 -0600

    test_echo: add echo program that reads/writes stdio

---
+++ resin/src/bin/test_echo.rs
@@ -0,0 +1,16 @@
+use std::io::{self, Read, Write};
+
+fn main() {
+	loop {
+		let mut buf = String::new();
+		io::stdin().read_line(&mut buf)
+			.expect("could not read line");
+
+		if buf.starts_with("exit") {
+			std::process::exit(0);
+		}
+
+		io::stdout().write(&buf.as_bytes())
+			.expect("could not write line");
+	}
+}
